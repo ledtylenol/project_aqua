c1f46b9b4669789e4041ce68f125c044e3b8891c
LedTylenol <ledtylenol@duck.com>
2024-12-14 09:38:01 +0200

    asdf

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,6 +1,6 @@
 extends Camera3D
 func _physics_process(delta: float) -> void:
-	var dir = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up")
+	var dir = Input.get_vector("left", "right", "down", "up")
 	position += basis * xy(dir) * delta
 	var rot = Input.get_axis("rotate_right", "rotate_left")
 	rotate_z(rot * delta * 2 * PI)
